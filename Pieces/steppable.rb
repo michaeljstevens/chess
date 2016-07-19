a61ec9cb7583a7ca6c82e9e09ceae8d27a0b0f89
App Academy Student <student@appacademy.io>
2016-07-19 10:49:14 -0700

    implemented the steppable module and make the king and knight pieces mobile

--- Pieces/steppable.rb
+++ Pieces/steppable.rb
@@ -1,2 +1,34 @@
-class SteppingPiece < Piece
+module Steppable
+
+KING_MOVES = [[1,0], [0,1], [1,1], [-1,0], [0,-1], [-1,-1], [1,-1], [-1,1]]
+KNIGHT_MOVES = [[2,1], [1,2], [-2,-1], [-1,-2], [-2,1], [2,-1], [-1,2], [1,-2]]
+
+def king_moves
+  KING_MOVES
+end
+
+def knight_moves
+  KNIGHT_MOVES
+end
+
+def moves()
+  possible_moves = []
+  move_dirs.each do |x, y|
+    current_x, current_y = position
+    position = [current_x + x, current_y + y]
+
+    next unless board.valid_pos?(position)
+
+    if board.empty?(position)
+      possible_moves << position
+    elsif board[position].color != color
+      possible_moves << position
+    end
+  end
+  possible_moves
+end
+
+
+
+
 end
